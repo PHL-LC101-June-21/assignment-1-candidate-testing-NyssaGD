20b7c4700c4300e95c854ab089c6f1545e53745c
Nyssa Smith <85656607+NyssaGD@users.noreply.github.com>
2021-06-29 01:52:38 +0000

    I fixed the questions to be a function, made the quiz grade itself

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,24 +3,24 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question;
 let correctAnswer = "Sally Ride";
 let candidateAnswer;
 let questions = [
-  "Who was the first American woman in space?",
-  "True or false: 5 kilometer == 5000 meters?",
-  "(5 + 3)/2 * 10 = ?",
-  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?",
-  "What is the minimum crew size for the ISS?"
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
 ];
-let correctAnswers = ["Sally Ride","True",40,"Trajectory",3];
+let correctAnswers = ["Sally Ride","True","40","Trajectory","3"];
 let candidateAnswers = [];
 
-function askForName(candidateName) {
+function askForName(name) {
   // TODO 1.1b: Ask for candidate's name //
- candidateName = input.question ("What is your name? ");
+ const candidateName = input.question ("What is your name? ");
   // TODO 1.1c: Say Hello to the candidate //
   console.log("Hello", candidateName,", welcome to the cosmic quiz! Make sure all written answers are capitalized and all numbers are numbers (not words). Good luck! ")
 }
@@ -28,58 +28,10 @@ function askForName(candidateName) {
 console.log("~*~*~*~*~*~*~\n")
 
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-  // 1) Who was the first American woman in space?
-  // Correct Answer: Sally Ride
-console.log("~*~*~*~*~*~*~\n")
-  candidateAnswer = input.question("Who was the first American woman in space? ");
-  correctAnswer = correctAnswers[0];
-  candidateAnswers = [candidateAnswer];
-
-  console.log(`Your answer was: ${candidateAnswer}\nThe correct answer: ${correctAnswer}`);
-
-console.log("~*~*~*~*~*~*~\n")
-
-  // 2) True or false: 5 kilometer == 5000 meters?
-  // Correct Answer: True
-  candidateAnswer = input.question("True or false: 5 kilometer == 5000 meters? ");
-  correctAnswer = correctAnswers[1];
-  candidateAnswers = candidateAnswers.concat(candidateAnswer);
-
-  console.log(`Your answer was: ${candidateAnswer}\nThe correct answer: ${correctAnswer}`);
-
-console.log("~*~*~*~*~*~*~\n")
-
-// 3) (5 + 3)/2 * 10 = ?
-// Correct Answer: 40
-  candidateAnswer = input.question("(5 + 3)/2 * 10 = ? ");
-  correctAnswer = correctAnswers[2];
-  candidateAnswers = candidateAnswers.concat(candidateAnswer);
-
-  console.log(`Your answer was: ${candidateAnswer}\nThe correct answer: ${correctAnswer}`);
-
-console.log("~*~*~*~*~*~*~\n")
-
-// 4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?
-// Correct Answer: Trajectory
- candidateAnswer = input.question("Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ");
-  correctAnswer = correctAnswers[3];
-  candidateAnswers = candidateAnswers.concat(candidateAnswer);
-
-  console.log(`Your answer was: ${candidateAnswer}\nThe correct answer: ${correctAnswer}`);
-
-console.log("~*~*~*~*~*~*~\n")
-
-// 5) What is the minimum crew size for the ISS?
-// Correct Answer: 3
- candidateAnswer = input.question("What is the minimum crew size for the ISS? ");
-  correctAnswer = correctAnswers[4];
-  candidateAnswers = candidateAnswers.concat(candidateAnswer);
-
-  console.log(`Your answer was: ${candidateAnswer}\nThe correct answer: ${correctAnswer}`);
-
-console.log("~*~*~*~*~*~*~\n")
+   for (let i = 0; i < questions.length; i++){
+    let collectedAnswers = input.question(questions[i]);
+     candidateAnswers.push(collectedAnswers);
+   }
 
 }
 
@@ -87,17 +39,48 @@ console.log("~*~*~*~*~*~*~\n")
 
 function gradeQuiz(candidateAnswers) {
   
-let grade = Number((candidateAnswers / questions.length) * 100);
-if (candidateAnswers == correctAnswers && grade >= 80){
-  console.log(`CONGRATS you passed! Your score is ${grade}%`)
-}else{
-  console.log(`Whomp waahhhh. You did not pass. Your score is ${grade}%`)
+
+let totalCorrect = 0;
+//Number((candidateAnswers / questions.length) * 100)
+for (let i = 0; i < questions.length; i++){
+  if (candidateAnswers[i].toLowerCase() == correctAnswers[i].toLowerCase()){
+   
+    totalCorrect = totalCorrect + 1;
+  }
 }
+let grade = totalCorrect/5 * 100;
+let candidateStatus="";
+console.log(grade)
+   if (grade >= 80){
+  candidateStatus = "passed";
+ } else {
+  candidateStatus = "failed";
+ }
+console.log(`Candidate Name: ${candidateName}
+1) Who was the first American woman in space?
+Your Answer: ${candidateAnswers[0]}
+Correct Answer: Sally Ride
 
-  return grade;
+2) True or false: 5000 meters = 5 kilometers.
+Your Answer: ${candidateAnswers[1]}
+Correct Answer: true
+
+3) (5 + 3)/2 * 10 = ?
+Your Answer: ${candidateAnswers[2]}
+Correct Answer: 40
+
+4) Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?
+Your Answer: ${candidateAnswers[3]}
+Correct Answer: Trajectory
+
+5) What is the minimum crew size for the ISS?
+Your Answer: ${candidateAnswers[4]}
+Correct Answer: 3
+
+>>> Overall Grade: ${grade}% (${totalCorrect} of 5 responses correct) <<<
+>>> Status: ${candidateStatus} <<<")`)
+
 }
-
-
 
 function runProgram() {
   askForName();
@@ -106,6 +89,7 @@ function runProgram() {
   askQuestion();
  gradeQuiz(this.candidateAnswers);
 }
+
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
